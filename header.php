fd7949d64872c2b58d26a61af87b296fe3183166
gadnestar <3510926@qq.com>
2015-07-15 18:16:12 +0800

    修改注释
    修改注释

--- header.php
+++ header.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * The header for our theme.
- *
- * Displays all of the <head> section and everything up till <div id="content">
- *
- * @package nz
- */
-
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <?php
