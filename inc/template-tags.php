4f04861d8c3fa4468b230f41936aa6f4635706d3
gadnestar <3510926@qq.com>
2015-08-12 17:50:04 +0800

    评论回调

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -37,6 +37,29 @@ function the_posts_navigation() {
 }
 endif;
 
+
+function mytheme_comment($comment, $args, $depth) {
+$GLOBALS['comment'] = $comment; ?>
+<li <?php comment_class(); ?> id="li-comment-<?php comment_ID() ?>">
+<div id="comment-<?php comment_ID(); ?>" class="comment-content uk-clearfix ">
+<div class="avatar hovercard"><?php echo get_avatar($comment,$size='24',$default='<path_to_url>' ); ?></div>
+<?php if ($comment->comment_approved == '0') : ?>
+<em><?php _e('Your comment is awaiting moderation.') ?></em>
+<br />
+<?php endif; ?>
+	<div class="comment-body">
+	<header>
+	<span class="comment-byline"><?php printf(__('%s '), get_comment_author_link()) ?></span>
+	<span class="comment-meta"><a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>"><?php printf(__(' - %1$s at %2$s'), get_comment_date(), get_comment_time()) ?></a><?php edit_comment_link(__('(Edit)'),' ','') ?></span>
+	</header>
+	<div class="comment-body-inner"><?php comment_text() ?></div>
+	<footer><?php comment_reply_link(array_merge( $args, array('depth' => $depth, 'max_depth' => $args['max_depth']))) ?></footer>
+		</div>
+	</div>
+<?php
+}
+
+
 if ( ! function_exists( 'the_post_navigation' ) ) :
 /**
  * Display navigation to next/previous post when applicable.
