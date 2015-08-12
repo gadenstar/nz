d3d5cc65466e300be6534f3a1bf9920ca31e2865
gadnestar <3510926@qq.com>
2015-08-12 17:49:08 +0800

    single结构更新

--- single.php
+++ single.php
@@ -1,20 +1,32 @@
 <?php
-/**
- * The template for displaying all single posts.
- *
- * @package nz
- */
+global $post,
+$mk_options;
+
+$single_layout = $mk_options['single_layout'];
+if ($single_layout!='full') {
+	if ($single_layout == 'left') {
+		$layout_css = 'uk-width-medium-2-3 uk-width-large-3-4 uk-flex-order-last';
+	} else {
+		$layout_css = 'uk-width-medium-2-3 uk-width-large-3-4';
+	}
+
+} else {
+	$layout_css = 'uk-width-1-1';
+}
+
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<div id="primary" class="content-area <?php echo $layout_css;?>">
+
+		<main id="main" class="site-main post-list" role="main">
+			<div class="post-content">
 
 		<?php while ( have_posts() ) : the_post(); ?>
 
 			<?php get_template_part( 'template-parts/content', 'single' ); ?>
-
-			<?php the_post_navigation(); ?>
+			<?php echo $single_layout;?>
+			<?php //the_post_navigation(); ?>
 
 			<?php
 				// If comments are open or we have at least one comment, load up the comment template.
@@ -24,9 +36,11 @@ get_header(); ?>
 			?>
 
 		<?php endwhile; // End of the loop. ?>
+				</div>
 
 		</main><!-- #main -->
+
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
+<?php  if($single_layout != 'full') get_sidebar();  ?>
 <?php get_footer(); ?>
